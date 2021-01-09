3f1379c0aa32b64cd24f43085dcac2696b82b286
Dmytro_Klymchuk <dmytro.klymchuk@smile-ukraine.com>
2021-01-09 16:21:41 +0200

    [vue] [keep-balance] optimization for components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
+import { createApp, defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import store from './store'
-import GamePopUp from "@/components/UI/GamePopUp";
-
+const GamePopUp = defineAsyncComponent(()=> import('@/components/UI/GamePopUp'));
 const app = createApp(App);
 app.use(store);
 app.component('game-popup', GamePopUp);
